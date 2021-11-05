ce7899bf5569bb2a0f9f851d803aa431214d2194
2016170240 <52840487+2016170240@users.noreply.github.com>
2021-11-05 10:27:54 +0200

    new commit

--- custom.js
+++ custom.js
@@ -1,39 +1,25 @@
-let date = new Date("Nov 30 ,2021 23:59:59").getTime();
+let finalDate = new Date("Dec 31 ,2021 23:59:59").getTime();
 
-/* 
-   this will print time in milli seconds 
-   if we divide it on 1000 will print date in seconds
-   date starts from 1970
-*/
-// get limit dat
+let countDown = setInterval(() => {
+  let currentDate = new Date().getTime();
 
-let count = setInterval(() => {
-    let currentDate = new Date().getTime();
-    console.log(currentDate);
-  
-    let dateDiff = date - currentDate;
-  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
-  let hourse = Math.floor(
-    (dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
+  let diffDate = finalDate - currentDate;
+  let dayes = Math.floor(diffDate / 1000 / 60 / 60 / 24);
+  let hourse = Math.floor(diffDate / 1000 / 60 / 60) % 24;
+  let minutes = Math.floor(diffDate / 1000 / 60) % 60;
+  let seconds = Math.floor(diffDate / 1000) % 60;
 
-  let minutes = Math.floor((dateDiff % (1000 * 60 * 60 )) / (1000 * 60));
-
-  let seconds = Math.floor((dateDiff % (1000 * 60 ) / 1000));
   let day = document.querySelector(".days");
-  day.innerHTML = days;
+  let houre = document.querySelector(".hourse");
+  let minut = document.querySelector(".minutes");
+  let second = document.querySelector(".seconds");
 
-  let hourse2 = document.querySelector(".hourse");
-  hourse2.innerHTML = hourse;
-
-  let minutes2 = document.querySelector(".minutes");
-  minutes2.innerHTML = minutes;
-
-  let seconds2 = document.querySelector(".seconds");
-  seconds2.innerHTML = seconds;
+  day.innerHTML = dayes < 10 ? `0${dayes}` : dayes;
+  houre.innerHTML = hourse < 10 ? `0${hourse}` : hourse;
+  minut.innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+  second.innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+  if (diffDate <= 0) {
+    clearInterval(countDown);
+    console.log("congratulations");
+  }
 }, 1000);
-
-
-if(dateDiff<=0){
-  clearInterval(count)
-}
